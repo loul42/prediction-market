5bf0060ffd11627481d08920ff70c35af3a59bd5
Louis <louis.lonjon@gmail.com>
2017-08-27 15:41:10 +0200

    OK

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,13 @@ module.exports = {
 	entry: {'app' : glob.sync("./app/js/*.js"),},
 	output: {
 		path: path.join(__dirname, "/build/app/js"),
-		filename: "[name].js"
+		filename: "[name]1.js"
 	},
 	module: {
 		loaders: []
-	}
+	},
+	devServer: {
+    	contentBase: './build/app',
+    	inline: true
+  },
 };
